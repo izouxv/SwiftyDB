9599698c57c5cf90b006057d64ced36ead907e31
zouxu <izouxv@gmail.com>
2016-11-07 08:50:34 +0800

    no message

--- SwiftyDB/SwiftyDB.h
+++ SwiftyDB/SwiftyDB.h
@@ -6,7 +6,9 @@
 //  Copyright © 2016 team.bz. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
+
+//#import <UIKit/UIKit.h>
 
 //! Project version number for SwiftyDB.
 FOUNDATION_EXPORT double SwiftyDBVersionNumber;

538baf50100af2490a0894e342b1a92e0789695b
zouxu <izouxv@gmail.com>
2017-02-16 14:53:51 +0800

    no message

--- SwiftyDBTests/Tests.swift
+++ SwiftyDBTests/Tests.swift
@@ -13,6 +13,8 @@ class SwiftyDBSpec: QuickSpec {
     override func spec() {        
         let documentsDir : String = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)[0]
         let path = documentsDir+"/test_database.sqlite"
-        let _ = try? FileManager.default.removeItem(atPath: path)
+        if FileManager.default.fileExists(atPath: path){ 
+            let _ = try! FileManager.default.removeItem(atPath: path)
+        }
     }
 }
